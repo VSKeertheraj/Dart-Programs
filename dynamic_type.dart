f0aeb6d4da20a43c378788a29895b38886cd6067
Keertheraj <121630418+VSKeertheraj@users.noreply.github.com>
2023-03-16 12:53:54 +0530

    Create dynamic_type.dart

---
+++ dynamic_type.dart
@@ -0,0 +1,9 @@
+void main(){
+	/*You can use this datatype if you're not sure of which datatype can be used*/
+	dynamic S="Hi!"; 
+	print(S);
+	dynamic R=34;
+	print(R);
+	dynamic W=56.3;
+	print(W);
+}
