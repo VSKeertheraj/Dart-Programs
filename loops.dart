50f7ee63737fccd519caa933d53726c2294ad3f4
Keertheraj <121630418+VSKeertheraj@users.noreply.github.com>
2023-03-16 20:17:28 +0530

    Create loops.dart

---
+++ loops.dart
@@ -0,0 +1,21 @@
+void main(){
+  int num=5;
+  //for loop
+  for(var i=num;i>=1;i--)
+  { 
+    print(i);
+  }
+  //for in loop
+  var names=['John','Shadow','Rocker']; 
+  for(var name in names)
+  {
+    print(name);
+  }
+  //while loop
+  while(num>=1)
+  {
+    print(num);
+    num--;
+
+  }
+}
