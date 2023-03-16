62a933ff075888f3134d822685bd7ce5e55f5e14
Keertheraj <121630418+VSKeertheraj@users.noreply.github.com>
2023-03-16 23:56:26 +0530

    Create type_conversion.dart

---
+++ type_conversion.dart
@@ -0,0 +1,15 @@
+void main(){
+  var a=2,c;
+  var b="4";// converting string to integer
+  c = a+int.parse(b);
+  print('$a + $b = $c');
+
+  var d=3,f;
+  var e='3.2';// converting int to double
+  f=d + double.parse(e);
+  print('$d + $e = $f');
+
+  var g=3,h='5',i;
+  i=g.toString()+h; // concatenate 
+  print('$g + $h = $i');
+}
