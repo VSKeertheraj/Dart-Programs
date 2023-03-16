cd5b4b41d55443097dc77752ce6d56173dffb71b
Keertheraj <121630418+VSKeertheraj@users.noreply.github.com>
2023-03-16 12:55:32 +0530

    Create list_type.dart

---
+++ list_type.dart
@@ -0,0 +1,19 @@
+void main(){
+	var myList=[1,2,3,4]; //create a list
+	print(myList);
+
+    myList.add(32); //add a single element to list
+    print(myList);
+
+    myList.addAll([32,43,56]); // add multiple elements to a list
+    print(myList);
+
+    myList.insert(2,34); // inserting a element in 2nd position
+    print(myList);
+    
+    myList.insertAll(4,[46,78,89]); // inserting multiple elements to the list at 4th Position 
+    print(myList);
+    
+
+
+}
