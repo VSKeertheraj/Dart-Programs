61e5a56851d9e5ca8ff913a1de46a07d36f8bc82
Keertheraj <121630418+VSKeertheraj@users.noreply.github.com>
2023-03-16 12:50:47 +0530

    Create Data_types.dart

---
+++ Data_types.dart
@@ -0,0 +1,13 @@
+void main(){
+	int x=3; // integer datatype 
+	print(x);  
+	double v=24.56; // double datatype
+	print(v);
+	String h="Hello"; //String datatype
+	print(h);
+	bool d=true; // boolean datatype
+	print(d);
+
+}
+
+/*Other than this we have list and map.We will discuss that in the following programs*/
