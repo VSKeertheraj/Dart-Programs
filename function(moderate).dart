9f6275acac0cf50ea3a483a09347e8c4a4fe855c
Keertheraj <121630418+VSKeertheraj@users.noreply.github.com>
2023-03-16 21:26:03 +0530

    Create function(moderate).dart

---
+++ function(moderate).dart
@@ -0,0 +1,8 @@
+void main(){
+  myFunc(String name) 
+  {
+     return "Hello $name";  // function
+  }
+ var c = myFunc("Geek!"); // Giving Value to the String
+ print(c); // printing the value of c
+}
