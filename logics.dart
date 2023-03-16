fc4863c272f5ee55aa36e8a2400caba3c608bdc1
Keertheraj <121630418+VSKeertheraj@users.noreply.github.com>
2023-03-16 20:47:17 +0530

    Create logics.dart

---
+++ logics.dart
@@ -0,0 +1,19 @@
+void main(){
+  
+  int x=4; //declaring x=4
+  
+  if(x==2) //check x=2
+  {   
+    print("It's 2"); 
+  }
+  
+  else if(x==6) //check x=6
+  {
+    print("It's 6");
+  }
+  
+  else //If the above logics are not satisfied print this
+  {
+    print("It's 4");
+  }
+}
