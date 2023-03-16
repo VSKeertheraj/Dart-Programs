1a4f966262bdc2f92027fc42e4c81ca125f18c32
Keertheraj <121630418+VSKeertheraj@users.noreply.github.com>
2023-03-16 18:35:34 +0530

    Create boolean_in_map_and_list.dart

---
+++ boolean_in_map_and_list.dart
@@ -0,0 +1,14 @@
+void main(){
+var myMap={'John':'Burger','Danny':'Sausage'}; //CREATING MAP
+var myList=[1,2,3,4,5]; // CREATING LIST
+print(myMap.isEmpty); //CHECKING WHETHER MAP IS EMPTY
+myMap.clear(); //CLEARING THE MAP AS WE MAKE IT EMPTY NOW
+print(myMap.isEmpty); // NOW CHECKING MAP IS EMPTY
+print(myMap.isNotEmpty); // NOW CHECKING MAP IS NOT EMPTY
+
+print(myList.isEmpty); // CHECKING WHETHER LIST IS EMPTY
+myMap.clear(); // CLEARING THE LIST
+print(myList.isEmpty); // CHECKING WHETHER THE LIST IS EMPTY
+print(myList.isNotEmpty); // CHECKING WHETHER THE LIST IS NOT EMPTY
+
+}
